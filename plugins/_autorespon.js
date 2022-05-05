20455cd5757b2d594c1bdb828c2f4b050c7af99a
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-06 01:52:42 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -15,7 +15,7 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? `${namabot} tidak aktif` : banned ? 'kamu dibanned' : `AlyaaXzy di sini, Btw ngapain manggil kangen ya bg?`,
+                isBanned ? `${namabot} tidak aktif` : banned ? 'kamu dibanned' : `BABIBOT By verdi di sini, Btw ngapain manggil kangen ya sayang😳💜?`,
                 footer,
                 isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
                 isBanned ? '.unban' : banned ? '.owner' : '.?',
